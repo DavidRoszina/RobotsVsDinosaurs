feb231cdef7c3a391e628a2b61b583afe7208c20
DavidRoszina <david.a.roszina@gmail.com>
2020-04-13 22:25:08 -0500

    Added intoduction to game start

--- RoszinaRobotsVsDinosaurs/Battlefield.cs
+++ RoszinaRobotsVsDinosaurs/Battlefield.cs
@@ -22,15 +22,19 @@ namespace RoszinaRobotsVsDinosaurs
         {
             {
                 Console.WriteLine("Welcome to Robots vs. Dinosaurs!");
+                Console.WriteLine("The Cylons are a Fleet of 3 Robots.");
+                Console.WriteLine("Dino Rampage is a Herd of 3 Dinosaurs.");
 
 
                 for (int i = 0; i < battleFleet.theRobots.Count; i++)
                 {
                     battleFleet.theRobots[i].Attack(battleHerd.theDinos[i]);
+
                 }
                 for (int j = 0; j < battleHerd.theDinos.Count; j++)
                 {
                     battleHerd.theDinos[j].Attack(battleFleet.theRobots[j]);
+
                 }
                 //battleFleet.robot1.health -= battleHerd.dino1.attackPower;
             }
